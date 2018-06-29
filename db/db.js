8b46a56e2a830ecf0da2f7cf556a7c3a68d53315
adam slack <adamslack@outlook.com>
2018-06-29 15:51:22 +0100

    oh my days... i need a nap

--- db/db.js
+++ db/db.js
@@ -246,7 +246,7 @@ class DB {
     async selectLocaleByID(companyID) {
         console.log(`Selecting Locale for ${companyID}`);
         try{
-            let ret = await this.query('select id from companies where id = $1', [companyID]);
+            let ret = await this.query('select locale_iso_6391 from companies where id = $1', [companyID]);
             if(ret.rowCount != 0) {
                 return ret[0].locale_iso_6391;
             }
